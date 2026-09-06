0139627146f8ea1d1f695e5d5516492b223965c9
Subaina Pervaiz <subainapervaiz818@gmail.com>
2026-09-06 20:50:33 +0500

    Branch A: Multiply features by 100 - 23L-4047

--- src/train-23L-4047.py
+++ src/train-23L-4047.py
@@ -12,11 +12,8 @@ data_path = os.path.join("data", "dataset.csv")
 print(f"Loading raw dataset from {data_path}...")
 df = pd.read_csv(data_path)
 
-# Line 15 modification in Branch 2:
-X = (df[['feature1', 'feature2']] - df[['feature1', 'feature2']].min()) / (df[['feature1', 'feature2']].max() - df[['feature1', 'feature2']].min()) # MinMax Scaling (<STUDENT_ID>)
+X = df[['feature1', 'feature2']] * 100 # Preprocessing Technique A (23L-4047)
 
-
-X = df[['feature1', 'feature2']]
 y = df['target']
 
 # 2. Train ML Model
