ab8f35f0136f9075c55d497e1a77ec2216c51d23
Subaina Pervaiz <subainapervaiz818@gmail.com>
2026-09-06 20:36:12 +0500

    Add data normalization step - 23L-4047

--- src/train-23L-4047.py
+++ src/train-23L-4047.py
@@ -12,6 +12,9 @@ data_path = os.path.join("data", "dataset.csv")
 print(f"Loading raw dataset from {data_path}...")
 df = pd.read_csv(data_path)
 
+# Line 15 modification in Branch 1:
+X = (df[['feature1', 'feature2']] - df[['feature1', 'feature2']].mean()) / df[['feature1', 'feature2']].std() # Normalization step (<STUDENT_ID>)
+
 X = df[['feature1', 'feature2']]
 y = df['target']
 
