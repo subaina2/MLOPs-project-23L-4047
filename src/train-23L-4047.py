0dcbbc5c7a77256878de9177efe41e99af2ce3a2
Subaina Pervaiz <subainapervaiz818@gmail.com>
2026-09-06 21:23:57 +0500

    Update train-23L-4047.py

--- src/train-23L-4047.py
+++ src/train-23L-4047.py
@@ -3,25 +3,25 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
-# Student ID Verification
+
 STUDENT_ID = "23L-4047"
 print(f"--- Starting Training Pipeline for Student ID: {STUDENT_ID} ---")
 
-# 1. Load Data from data/ directory
+# load data from data/ directory
 data_path = os.path.join("data", "dataset.csv")
 print(f"Loading raw dataset from {data_path}...")
 df = pd.read_csv(data_path)
 
-X = df[['feature1', 'feature2']] * 100 # Preprocessing Technique A (23L-4047)
+X = df[['feature1', 'feature2']] * 100 
 
 y = df['target']
 
-# 2. Train ML Model
+# train ML Model
 print("Training RandomForest model...")
 model = RandomForestClassifier(n_estimators=10, random_state=42)
 model.fit(X, y)
 
-# 3. Serialize and Save Trained Model into model/ directory
+# save trained model into model/ directory
 os.makedirs("model", exist_ok=True)
 model_output_path = os.path.join("model", f"model_{STUDENT_ID}.pkl")
 joblib.dump(model, model_output_path)
